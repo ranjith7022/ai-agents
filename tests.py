8cc43bab73684315aff2bd8837b70582a9e7a6cd
Ranjith <mailranjith7022@gmail.com>
2025-06-26 22:05:05 +0530

    ai-agents initial commits

---
+++ tests.py
@@ -0,0 +1,8 @@
+# from subdirectory.filename import function_name
+from functions.get_files_info import get_files_info,get_file_content,write_file
+from functions.run_python import run_python_file
+
+
+
+
+
